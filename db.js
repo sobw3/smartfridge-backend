219149a5ac4e1ac6dcaa2a2eb35ce180af7dea87
Messias Emanoel <messiascunha09@gmail.com>
2025-07-02 17:47:04 -0300

    Versão estável e final do backend

--- db.js
+++ db.js
@@ -4,6 +4,7 @@ require('dotenv').config();
 const connectionString = process.env.DATABASE_URL;
 
 if (!connectionString) {
+    console.error('ERRO CRÍTICO: Variável de ambiente DATABASE_URL não está definida!');
     throw new Error("A variável de ambiente DATABASE_URL não está definida.");
 }
 
