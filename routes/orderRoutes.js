cb43a38d918c4535d7c69b1a901205b0990367ea
sobw3 <messiascunha09@gmail.com>
2025-07-02 16:09:26 -0300

    Add files via upload

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -3,9 +3,7 @@ const router = express.Router();
 const orderController = require('../controllers/orderController');
 const { protect } = require('../middleware/authMiddleware');
 
-// A correção está aqui: era createOrder, agora é createPixOrder
-router.post('/create-pix', orderController.createPixOrder); 
-
+router.post('/create-pix', orderController.createPixOrder);
 router.post('/create-card', protect, orderController.createCardOrder);
 router.post('/:orderId/simulate-payment', orderController.simulatePaymentApproval);
 router.get('/:orderId/status', protect, orderController.getOrderStatus);
