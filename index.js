329ec54dc4abd8f5e539e09a8795a176ab0cfe95
sobw3 <messiascunha09@gmail.com>
2025-07-02 14:09:50 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -12,16 +12,15 @@ const webhookRoutes = require('./routes/webhookRoutes');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// Configuração de CORS para aceitar pedidos do seu frontend
 const corsOptions = {
     origin: process.env.CORS_ORIGIN,
     optionsSuccessStatus: 200
 };
 app.use(cors(corsOptions));
+app.options('*', cors(corsOptions)); 
 
 app.use(express.json());
 
-// Rotas da API
 app.use('/api/auth', authRoutes);
 app.use('/api/products', productRoutes);
 app.use('/api/admin', adminRoutes);
@@ -35,4 +34,4 @@ app.get('/', (req, res) => {
 
 app.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`);
-})
+});
