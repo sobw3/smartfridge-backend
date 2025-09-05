e7d5e708260399daec13054d63260018fbd607c8
sobw3 <messiascunha09@gmail.com>
2025-09-04 23:48:31 -0300

    Update fridgeRoutes.js

--- routes/fridgeRoutes.js
+++ routes/fridgeRoutes.js
@@ -5,7 +5,7 @@ const { protectFridge } = require('../middleware/authMiddleware');
 
 
 // Rota que a geladeira irá chamar para verificar se há um token de desbloqueio
-router.post('/check-unlock', protectFridge, fridgeController.checkUnlock);
 router.get('/poll-unlock/:fridgeId', protectFridge, fridgeController.pollForUnlock);
 
 module.exports = router;
+
